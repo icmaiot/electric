dc27a22c0bf756c60ba9c78eef051a9500790012
Miguel Miranda Colis <44578094+CrowliDz@users.noreply.github.com>
2022-08-19 23:19:58 -0500

    cc

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  // urlEndPoint: 'http://54.241.14.55:5000'
-  urlEndPoint: 'http://localhost:5000' ,
+   urlEndPoint: 'http://54.241.14.55:5000'
+  //urlEndPoint: 'http://localhost:5000' ,
   urlMQTT: 'http://54.241.14.55:3000'
 };
